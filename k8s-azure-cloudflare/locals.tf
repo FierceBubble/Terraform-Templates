ac9238df804bf8a07faa891f4c5bad239597bc3f
Daniel Ryan <83863427+FierceBubble@users.noreply.github.com>
2023-06-29 21:47:43 +0800

    Delete locals.tf

--- k8s-azure-cloudflare/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  root_dir        = abspath(path.root)
-  k8s_config_dir  = "${local.root_dir}/.kube"
-  k8s_config_file = "${local.k8s_config_dir}/kubeconfig.yaml"
-  public_key_dir  = "/Users/fiercebubble/.ssh/azure_key.pub"
-}
