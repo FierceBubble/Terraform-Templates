ab5d63d11c4f9e8c1ad52ec74da3fac5445793ae
Daniel Ryan <83863427+FierceBubble@users.noreply.github.com>
2023-06-29 21:48:07 +0800

    Delete locals.tf

--- k8s-linode-cloudflare/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  root_dir        = abspath(path.root)
-  k8s_config_dir  = "${local.root_dir}/.kube"
-  k8s_config_file = "${local.k8s_config_dir}/kubeconfig.yaml"
-  linode_ids      = linode_lke_cluster.tf-k8s-cluster.pool[0].nodes[*].instance_id
-}
